16a2d4e0f5082329281288255a5387580b2e7245
cqc <chao373737@gmail.com>
2024-02-17 18:16:15 +0800

    btcnode sh

---
+++ btcfullnode.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo apt update;
+sudo apt install snapd;
+snap install bitcoin-core;
+
+RPC_USER=$1
+RPC_PASSWORD=$2
+RPC_PORT=$3
+
+nohup bitcoin-core.daemon -txindex=1 -server=1 -datadir="/root/btc" -rpcworkqueue=64 -rpcuser=$RPC_USER -rpcpassword=$RPC_PASSWORD -rest -rpcbind=0.0.0.0:$RPC_PORT -rpcallowip=0.0.0.0/0 &
+
+tail -1000f nohup.out
