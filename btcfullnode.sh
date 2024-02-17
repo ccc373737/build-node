c28281ceb79edac2f585118d9a88f4051e22ce84
cqc <chao373737@gmail.com>
2024-02-17 18:33:16 +0800

    btc

--- btcfullnode.sh
+++ btcfullnode.sh
@@ -7,6 +7,8 @@ RPC_USER=$1
 RPC_PASSWORD=$2
 RPC_PORT=$3
 
+mkdir /root/btc
+
 nohup bitcoin-core.daemon -txindex=1 -server=1 -datadir="/root/btc" -rpcworkqueue=64 -rpcuser=$RPC_USER -rpcpassword=$RPC_PASSWORD -rest -rpcbind=0.0.0.0:$RPC_PORT -rpcallowip=0.0.0.0/0 &
 
 tail -1000f nohup.out
