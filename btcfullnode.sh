14d77baa346577b3d51e49a787429ef9f586a0a7
cqc <chao373737@gmail.com>
2024-02-19 22:29:04 +0800

    btc

--- btcfullnode.sh
+++ btcfullnode.sh
@@ -9,4 +9,8 @@ RPC_PORT=$3
 
 mkdir /root/btc
 
-nohup bitcoin-core.daemon -txindex=1 -server=1 -datadir="/root/btc" -rpcworkqueue=64 -rpcuser=$RPC_USER -rpcpassword=$RPC_PASSWORD -rest -rpcbind=0.0.0.0:$RPC_PORT -rpcallowip=0.0.0.0/0 &
+sudo apt install -y screen
+
+screen
+
+bitcoin-core.daemon -txindex=1 -server=1 -datadir="/root/btc" -rpcworkqueue=64 -rpcuser=$RPC_USER -rpcpassword=$RPC_PASSWORD -rest -rpcbind=0.0.0.0:$RPC_PORT -rpcallowip=0.0.0.0/0
